9fadc430ec7d81bc868b48e0ab4e2c11265a779c
tsihnavy99 <tsihnavy99@naver.com>
2023-06-08 01:20:09 +0900

    v9

--- src/componenets/Movie.js
+++ src/componenets/Movie.js
@@ -5,7 +5,7 @@ function Movie({id, key, medium_cover_image, title, summary, genres}) {
     return (
         <div key={key}>
             <img src={medium_cover_image} alt={title}/>
-            <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+            <h2><Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link></h2>
             <p>{summary}</p>
             <ul>
             {genres.map(g => <li key={g}>{g}</li>)}
